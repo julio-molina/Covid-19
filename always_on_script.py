3f63f05b7fc655759c251085f8936334126c9efe
julio-molina <71189904+julio-molina@users.noreply.github.com>
2020-09-13 22:29:13 -0400

    Update always_on_script.py
    Includes the ability to run jupyter labs on linux.

--- always_on_script.py
+++ always_on_script.py
@@ -1,5 +1,15 @@
+#Julio Molina
+
+#NOTICE: currently using temporary hard coded directories to download and run jupyter notebooks.
+# ATM this script and it's use of additional programs currently implies that you are
+#using a linux based system, with "jupyter lab" and anaconda installed which includes many
+#data science oriented python libaries. Python 3.8
+
+#TODO: ATM the script is able to run everything atleast once, and not countious as it will 
+#be implemented later in its lifespan.
 from time import strftime, localtime, sleep
 import urllib.request
+import subprocess
 
 def gitDownloader():
     import urllib.request
@@ -22,14 +32,18 @@ def gitDownloader():
     #sleep(82800)#23 hours
     sleep(60)
 
+def runjupyter():
+    subprocess.call("jupyter lab ~/projects/dir/models.ipynb", shell=True)
 
 def main():
+    subprocess.call("cd ~/projects/dir/", shell=True)
     refreshTime="06:04"
     currentTime=strftime( "%H:%M",localtime())
     while(True):
         if (True):
             print("worked, do something ")
             gitDownloader();
+            runjupyter();
         else:
             print("Nothin")
             sleep(60)
