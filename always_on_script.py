0f590d18e155922e3a9c273a7f244a1687d58c4b
Julio Molina <71189904+julio-molina@users.noreply.github.com>
2020-09-15 17:17:57 -0400

    Allowed runJupyter func to be more continuous
    Changed to UTC time, will come in handy to handle update checks and operations. 
    Increased runjupyter function to be called recursively.

--- always_on_script.py
+++ always_on_script.py
@@ -5,9 +5,9 @@
 #using a linux based system, with "jupyter lab" and anaconda installed which includes many
 #data science oriented python libaries. Python 3.8
 
-#TODO: ATM the script is able to run everything atleast once, and not countious as it will 
+#TODO: ATM the script is able to run everything atleast once, and not countious as it will
 #be implemented later in its lifespan.
-from time import strftime, localtime, sleep
+from time import strftime, sleep, gmtime#UTC time +4EST
 import urllib.request
 import subprocess
 
@@ -30,17 +30,23 @@ def gitDownloader():
     print ("download file location: ", filename)
     print ("download headers: ", headers)
     #sleep(82800)#23 hours
-    sleep(60)
+    sleep(6)
 
 def runjupyter():
-    subprocess.call("jupyter lab ~/projects/dir/models.ipynb", shell=True)
+    #process=subprocess.call("jupyter lab ~/projects/dir/models.ipynb", shell=True)
+    #command = ['perl', '-e', "'alarm shift @ARGV; exec @ARGV'", "200"]
+    exec_proc = subprocess.call("perl -e 'alarm shift @ARGV; exec @ARGV' 21600 jupyter lab ~/projects/dir/models.ipynb", shell=True)# 6 hours
+
+
+def getFilestats():
+    print("set to do nothing:")
+
 
 def main():
     subprocess.call("cd ~/projects/dir/", shell=True)
-    refreshTime="06:04"
-    currentTime=strftime( "%H:%M",localtime())
+    currentTime=strftime( "%H:%M",gmtime())#UTC time +4 EST
     while(True):
-        if (True):
+        if (True):#("10:00"):#utctime +4 EST
             print("worked, do something ")
             gitDownloader();
             runjupyter();
